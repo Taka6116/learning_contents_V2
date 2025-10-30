8d1b41b751e9a951512bd1f63a9f871c736a5f6f
Taka6116 <goto_takashi@cellmuller.com>
2025-10-30 11:25:52 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,7 +4,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAzSZQUvIOIzqGi198mzxB-qYjYGC9UNes",
   authDomain: "learning-app-ccfd7.firebaseapp.com",
   // Realtime DatabaseのURLは、プロジェクトIDから手動で構築されます
-  databaseURL: "https://learning-app-ccfd7-default-rtdb.firebaseio.com",
+  databaseURL: "https://learning-app-ccfd7-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "learning-app-ccfd7",
   storageBucket: "learning-app-ccfd7.firebasestorage.app",
   messagingSenderId: "311519234667",
@@ -16,3 +16,4 @@ firebase.initializeApp(firebaseConfig);
 
 // データベース参照 (Realtime Databaseへの参照)
 const database = firebase.database();
+
